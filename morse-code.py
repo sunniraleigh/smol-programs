197d3a2bdeaf1436fddf2d8dfaa7782db7c3a7b1
sunniraleigh <sraleigh@smith.edu>
2023-02-20 08:28:56 -0500

    create morse-code file and add translations to script

---
+++ morse-code.py
@@ -0,0 +1,49 @@
+# A python program that translates phrases into morse code
+# Inspired by the first chapter of Charles Petzold's Code
+
+# PSEUDO CODE
+# receive input from user
+# iterate over user statement
+# print to terminal corresponding morse code character
+
+# store morse code translations in a dictionary
+morse = {
+  "a":"*-",
+  "b":"-***",
+  "c":"-*-*",
+  "d":"-**",
+  "e":"*",
+  "f":"**-*",
+  "g":"--*",
+  "h":"****",
+  "i":"**",
+  "j":"*---",
+  "k":"-*-",
+  "l":"*-**",
+  "m":"--",
+  "n":"-*",
+  "o":"---",
+  "p":"*--*",
+  "q":"--*-",
+  "r":"*-*",
+  "s":"***",
+  "t":"-",
+  "u":"**-",
+  "v":"***-",
+  "w":"*--",
+  "x":"-**-",
+  "y":"-*--",
+  "z":"--**",
+  "1":"*----",
+  "2":"**---",
+  "3":"***--",
+  "4":"****-",
+  "5":"*****",
+  "6":"-****",
+  "7":"--***",
+  "8":"---**",
+  "9":"----*",
+  "0":"-----"
+}
+
+
